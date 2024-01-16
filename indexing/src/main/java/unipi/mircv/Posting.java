801483df8478b6dd43ff5e3a6d7456a70b4a82d9
vittoria <vitty.acampora@gmail.com>
2024-01-16 13:08:51 +0100

    class posting

--- indexing/src/main/java/unipi/mircv/Posting.java
+++ indexing/src/main/java/unipi/mircv/Posting.java
@@ -1,11 +1,20 @@
 package unipi.mircv;
 
+/**
+ * Represents a posting in the inverted index, containing information about a document ID and its term frequency.
+ */
+
 public class Posting {
 
-    private long docID;
-    private int termFrequency;
+    private long docID; // Document ID associated with the posting
+    private int termFrequency; // Term frequency of the term in the document
 
 
+    /**
+     * Constructs a Posting with the specified document ID and term frequency.
+     * @param docID The document ID associated with the posting.
+     * @param termFrequency The term frequency of the term in the document.
+     */
     public Posting(long docID, int termFrequency) {
         this.docID = docID;
         this.termFrequency = termFrequency;
@@ -29,6 +38,10 @@ public class Posting {
 
     public void updateTermFrequency() { termFrequency++; }
 
+    /**
+     * Returns a string representation of the Posting object.
+     * @return A string containing the document ID and term frequency.
+     */
     @Override
     public String toString() {
         return "Posting{" +
